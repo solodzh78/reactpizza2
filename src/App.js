3d3963fb655c688bd68403a9f01c1389a9929f45
Sergei Solodzhuk <solodzh@gmail.com>
2022-05-21 04:28:34 +0200

    первый коммит

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Categories } from './components/Categories';
+import { Header } from './components/Header';
+import { PizzaBlock } from './components/PizzaBlock';
+import { Sort } from './components/Sort';
+import './scss/app.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories />
+                        <Sort />
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        {[...Array(9)].map((elem, index) => 
+                            <PizzaBlock
+                                key={index}
+                                title={'Марнарита'}
+                                price={230}
+                            />)}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
